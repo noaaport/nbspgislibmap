e9ee3b53e58deab9e86886bb7354e9c203cd2521
Jose F Nieves <nieves@volkron.net>
2011-08-14 22:21:18 -0400

    Include the tests as examples, and revise the debian packaging scripts

---
+++ doc/examples/ex1-bref.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+usage="Usage example: ex1-bref jua n0r"
+
+# config
+server="www.opennoaaport.net:8015"
+listurl="http://$server/_export/query_dir?dir=digatmos/nexrad/nids"
+geturl="http://$server/_export_get/digatmos/nexrad/nids"
+
+#
+# main
+#
+[ $# -ne 2 ] && { echo "Needs a site and type. E.g., $usage"; exit 1; }
+site=$1
+type=$2
+
+nidsfile="${type}${site}_latest.nids"
+
+mkdir -p ${type}${site}
+cd ${type}${site}
+
+echo "Downloading $nidsfile ..."
+wget -q -O $nidsfile $geturl/$site/$type/latest
+[ $? -ne 0 ] && { echo "Error from wget"; rm -f $nidsfile; exit 1; }
+
+echo "Generating shapefile and png ..."
+nbspglradmap -k $nidsfile
---
+++ doc/examples/ex2-bref.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+usage="Usage example: ex2-bref.sh n0r"
+
+# config
+server="www.opennoaaport.net:8015"
+listurl="http://$server/_export/query_dir?dir=digatmos/nexrad/nids"
+geturl="http://$server/_export_get/digatmos/nexrad/nids"
+
+#
+# main
+#
+[ $# -eq 0 ] && { echo $usage; exit 1; }
+type=$1
+
+mkdir -p ${type}
+cd ${type}
+
+# tx: ama bro crp dfx dyx ewx fws grk hgx lbb maf sjt
+# ok: fdr inx tlx vnx
+
+for site in ama bro crp dfx dyx ewx fws grk hgx lbb maf sjt
+do 
+    nidsfile="${type}${site}_latest.nids"
+    echo "Downloading $nidsfile ..."
+    wget -q -O $nidsfile $geturl/$site/$type/latest
+    [ $? -ne 0 ] && { echo "Error from wget"; rm -f $nidsfile; exit 1; }
+done
+
+echo "Generating shapefile and png ..."
+# nbspglradmap -k *.nids
+nbspglradmap *.nids
